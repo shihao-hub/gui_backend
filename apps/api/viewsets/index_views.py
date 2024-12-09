69cfaa7d807670353225347c53d4199689dcfb27
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-09 17:39:36 +0800

    Update index_views.py

--- apps/api/viewsets/index_views.py
+++ apps/api/viewsets/index_views.py
@@ -1,6 +1,11 @@
 from ninja import Router
 
-from apps.api.views import api
+from apps.api.shared.singleton import Singleton
+from apps.core.shared.log import Log
+
+api = Singleton.api
+log = Log()
+
 
 router = Router(tags=["index"])
 api.add_router("", router)

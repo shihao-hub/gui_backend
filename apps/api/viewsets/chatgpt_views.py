e5c94790c90272d0e9d52bf059b85b7a4b1be7cd
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-09 17:39:28 +0800

    Update chatgpt_views.py

--- apps/api/viewsets/chatgpt_views.py
+++ apps/api/viewsets/chatgpt_views.py
@@ -1,6 +1,10 @@
 from ninja import Router
 
-from apps.api.views import api
+from apps.api.shared.singleton import Singleton
+from apps.core.shared.log import Log
+
+api = Singleton.api
+log = Log()
 
 router = Router(tags=["chatgpt"])
 api.add_router("/chatgpt", router)

06e5b4002f545c9982126301ceeb607676014fa4
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-10 21:31:55 +0800

    Update settings.py

--- gui_backend/settings.py
+++ gui_backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+# import logfire
+
+# logfire.configure()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

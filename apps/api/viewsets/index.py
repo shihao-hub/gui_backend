0980b654e9e087faa93e1d8de844270cd3a16eb3
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-10 21:28:32 +0800

    Update and rename index_views.py to index.py

--- apps/api/viewsets/index.py
+++ apps/api/viewsets/index.py
@@ -1,11 +1,10 @@
 from ninja import Router
 
 from apps.api.shared.singleton import Singleton
+from apps.api.shared.utils import generic_response, get_registered_router
 from apps.core.shared.log import Log
 
 api = Singleton.api
 log = Log()
 
-
-router = Router(tags=["index"])
-api.add_router("", router)
+router = get_registered_router(api, __file__)

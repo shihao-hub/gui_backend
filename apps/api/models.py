f40b3edfafa5937314f53a4dcc5be40eaa22055e
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-10 21:30:22 +0800

    Update models.py

--- apps/api/models.py
+++ apps/api/models.py
@@ -4,3 +4,24 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)  # 13 位 ISBN
+    pages = models.PositiveIntegerField()
+    language = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# mongodb
+# -------------------------------------------------------------------------------------------------------------------- #
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# redis
+# -------------------------------------------------------------------------------------------------------------------- #

3e6c1237e7b2e02fd12dab7b32a6a401e4d6816d
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-12 20:59:07 +0800

    Update urls.py

--- gui_backend/urls.py
+++ gui_backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from gui_backend.api import api
+from apps.api.router import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),

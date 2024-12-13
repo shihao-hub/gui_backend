3d54e0b57adc6c9e3d34e8357ff9697ffa8a72a9
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-13 17:51:22 +0800

    Update exceptions.py

--- apps/api/exceptions.py
+++ apps/api/exceptions.py
@@ -4,5 +4,6 @@ class ServiceUnavailableError(Exception):
 
 class BadRequestError(Exception):
     # TODO: 待完善
-    def __init__(self, data):
+    def __init__(self, message, data=None):
+        super().__init__(message)
         self.data = data

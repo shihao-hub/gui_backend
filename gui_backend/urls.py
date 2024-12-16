27641f1ff56c1bfbe490ce974214830e01f9a246
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-16 20:45:10 +0800

    Update urls.py

--- gui_backend/urls.py
+++ gui_backend/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from apps.api.router import api
 
 urlpatterns = [
-    path("", include("apps.index.urls"), name="django-index"),
+    path("index/", include("apps.index.urls"), name="django-index"),
+    path("api/", api.urls),
 
-    path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),  # 添加 allauth 路由
 
-    path('api/', api.urls),
+    path("admin/", admin.site.urls),
+
 ]

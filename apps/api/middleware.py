4957d58ec7fd153ed78b952d39dfc0d9caf17847
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-09 17:34:22 +0800

    Update middleware.py

--- apps/api/middleware.py
+++ apps/api/middleware.py
@@ -1,3 +1,30 @@
+"""
+from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpRequest
+
+from apps.api.settings import redis_db as db
+from apps.core.shared.log import Log
+
+log = Log()
+
+
+class RequestCountMiddleware(MiddlewareMixin):
+
+    @staticmethod
+    def get_counter_key(path: str) -> str:
+        return "RequestCountMiddleware:" + path
+
+    def process_request(self, request: HttpRequest):
+        this = self
+
+        key = self.get_counter_key(request.path)
+        val = db.get(key)
+        if val is None:
+            db.set(key, 1)
+        else:
+            db.set(key, int(val) + 1)  # the type of val is bytes, so we need to convert it to int
+"""
+
 from django.utils.deprecation import MiddlewareMixin
 
 from apps.api.settings import redis_db as db

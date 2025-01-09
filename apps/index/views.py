b2f4770a7b11996712cf55f6856b499d2c49d6db
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-09 17:35:17 +0800

    Update views.py

--- apps/index/views.py
+++ apps/index/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-# NOTE:
-#   测试发现，django-allauth 多半是个远古库。或者是国外用的。反正我这边用不了。
-
 class TodoListView(TemplateView):
     template_name = str(Path("todolist/todolist.html"))
 

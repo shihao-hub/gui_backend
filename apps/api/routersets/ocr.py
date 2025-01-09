6eaad89b9951f4ff7f2da498c1b40fac7b75b77a
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-09 17:31:25 +0800

    Update ocr.py

--- apps/api/routersets/ocr.py
+++ apps/api/routersets/ocr.py
@@ -1,6 +1,12 @@
+import uuid
 from typing import List
 
+from PIL import Image
+import pytesseract
+
+from django.conf import settings
 from django.http import HttpRequest
+from django.core.files.storage import FileSystemStorage
 from ninja import Router, Form, File, UploadedFile
 from ninja_extra import api_controller, http_get, http_post, http_put, http_delete
 
@@ -16,4 +22,10 @@ router = Router(tags=["ocr"])
 
 @router.post("/orc_one_image", response=generic_response, summary="ocr 一张图片")
 def orc_one_image(request: HttpRequest, image: File[UploadedFile]):
-    pass
+    location = str(settings.BASE_DIR / f"temporary")
+    fs = FileSystemStorage(location=location)
+    filename = f"{uuid.uuid4()}-{image.name}"
+    fs.save(filename, image.file)
+    image = Image.open(fs.path(filename))
+    text = pytesseract.image_to_string(image, lang="chi_sim")
+    return {"data": text}

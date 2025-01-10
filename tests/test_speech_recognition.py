8d5e8cf4d94e5524e8eec5e938a78f1080e76a5b
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-10 17:44:19 +0800

    Update test_speech_recognition.py

--- tests/test_speech_recognition.py
+++ tests/test_speech_recognition.py
@@ -5,7 +5,7 @@ import speech_recognition as sr
 # 创建语音识别的识别器实例
 recognizer = sr.Recognizer()
 # 打开 WAV 文件
-with sr.AudioFile("./output.wav") as source:
+with sr.AudioFile("./resources/media/recording.wav") as source:
     audio_data = recognizer.record(source)  # 读取音频文件
 # 使用Google Web服务进行识别
 try:
@@ -15,4 +15,4 @@ except sr.UnknownValueError as e:
     print(f"{e}\n{traceback.format_exc()}")
     print("未能理解音频")
 except sr.RequestError as e:
-    print(f"无法请求结果; {e}")
+    print(f"无法请求结果，原因：\n{traceback.format_exc()}")
